210ecc618b25cd45587e5e360e7c416062a85291
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-26 20:32:10 -0500

    chore(ui): Added PrimeVue UI Library first set of Components to Use.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import PrimeVue from "primevue/config";
 import "primevue/resources/themes/md-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
+import Sidebar from "primevue/sidebar";
+import Row from "primevue/row";
 
 const app = createApp(App);
 
@@ -14,5 +21,14 @@ app.use(router);
 
 // PrimeVue Configuration
 app.use(PrimeVue, { ripple: true });
+
+// PrimeVue Components
+app.component("pv-data-table", DataTable);
+app.component("pv-column", Column);
+app.component("pv-button", Button);
+app.component("pv-toolbar", Toolbar);
+app.component("pv-input-text", InputText);
+app.component("pv-sidebar", Sidebar);
+app.component("pv-row", Row);
 
 app.mount("#app");
