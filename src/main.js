0b8d280bc4ef24a62a25da85d1dd3c7161e18673
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-26 20:24:19 -0500

    chore(ui): Added PrimeVue UI Library with Material Design Theme.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
 
-const app = createApp(App)
+// PrimeVue Styles
+import "primevue/resources/themes/md-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+// PrimeVue Configuration
+app.use(PrimeVue, { ripple: true });
+
+app.mount("#app");
